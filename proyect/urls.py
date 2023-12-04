8fee4e94b844e5181de8c0c88acb660aa466c85c
rodrimaz <rodrigoimaz14gmail.com>
2023-12-04 11:43:53 -0300

    actualizacion settings, urls

---
+++ proyect/urls.py
@@ -0,0 +1,40 @@
+"""proyecto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+    path('carga_datos/', views.create_task, name='carga_datos'),
+    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
+    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    path('molineda/', views.molienda, name='molienda'),
+    path('exito/', views.exito, name='exito'),
+    path('licuefac/', views.v_licuefac, name='licuefac'),
+    path('reportar_incidente/', views.incidente_form, name='incidente_form'),
+    path('ingresar_datos/', views.ingresar_datos, name='ingresar_datos'),
+    path('last_five_rows/', views.last_five_rows, name='last_five_rows'),
+    path('enviar_correo/', views.enviar_correo, name='enviar_correo'),
+    path('programar_apertura/', views.programar_apertura, name='programar_apertura'),
+]
